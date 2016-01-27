8ad78992289712d556104fbb5507aa0019c0f4c2
Abdelrahman Mahmoud <mahmoud@affectiva.com>
2016-01-27 15:02:31 -0500

    display dominant emojis

--- CameraDetectorDemo/src/main/java/com/affectiva/cameradetectordemo/MainActivity.java
+++ CameraDetectorDemo/src/main/java/com/affectiva/cameradetectordemo/MainActivity.java
@@ -1,6 +1,7 @@
 package com.affectiva.cameradetectordemo;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Gravity;
@@ -24,6 +25,11 @@ import java.io.ByteArrayInputStream;
 import java.io.InputStreamReader;
 import java.util.List;
 
+// Add this to the header of your file:
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+
 /**
  * This is a very bare sample app to demonstrate the usage of the CameraDetector object from Affectiva.
  * It displays statistics on frames per second, percentage of time a face was detected, and the user's smile score.
@@ -37,14 +43,17 @@ public class MainActivity extends Activity implements Detector.ImageListener, Ca
     final String LOG_TAG = "Affectiva";
 
     Button startSDKButton;
-    Button surfaceViewVisibilityButton;
+    //Button surfaceViewVisibilityButton;
+    Button shareButton;
     TextView smileTextView;
-    ToggleButton toggleButton;
+    //ToggleButton toggleButton;
+    Face.EMOJI theEmoji = Face.EMOJI.UNKNOWN;
 
     SurfaceView cameraPreview;
 
     boolean isCameraBack = false;
     boolean isSDKStarted = false;
+    boolean screenTapped = false;
 
     RelativeLayout mainLayout;
 
@@ -55,21 +64,24 @@ public class MainActivity extends Activity implements Detector.ImageListener, Ca
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         smileTextView = (TextView) findViewById(R.id.smile_textview);
 
-        toggleButton = (ToggleButton) findViewById(R.id.front_back_toggle_button);
-        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        //toggleButton = (ToggleButton) findViewById(R.id.front_back_toggle_button);
+        shareButton = (Button) findViewById(R.id.share_button);
+        /*toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 isCameraBack = isChecked;
                 switchCamera(isCameraBack? CameraDetector.CameraType.CAMERA_BACK : CameraDetector.CameraType.CAMERA_FRONT);
             }
-        });
+        });*/
 
-        startSDKButton = (Button) findViewById(R.id.sdk_start_button);
+        /*startSDKButton = (Button) findViewById(R.id.sdk_start_button);
         startSDKButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -84,7 +96,22 @@ public class MainActivity extends Activity implements Detector.ImageListener, Ca
                 }
             }
         });
-        startSDKButton.setText("Start Camera");
+        startSDKButton.setText("Start Camera");*/
+
+        shareButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT, theEmoji.getUnicode());
+                sendIntent.setType("text/plain");
+                startActivity(sendIntent);
+                //startActivity(Intent.createChooser(sendIntent, theEmoji.getUnicode()));
+
+            }
+
+        });
 
         //We create a custom SurfaceView that resizes itself to match the aspect ratio of the incoming camera frames
         mainLayout = (RelativeLayout) findViewById(R.id.main_layout);
@@ -116,11 +143,11 @@ public class MainActivity extends Activity implements Detector.ImageListener, Ca
         RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         params.addRule(RelativeLayout.CENTER_IN_PARENT,RelativeLayout.TRUE);
         cameraPreview.setLayoutParams(params);
-        mainLayout.addView(cameraPreview,0);
+        mainLayout.addView(cameraPreview, 0);
 
-        surfaceViewVisibilityButton = (Button) findViewById(R.id.surfaceview_visibility_button);
-        surfaceViewVisibilityButton.setText("HIDE SURFACE VIEW");
-        surfaceViewVisibilityButton.setOnClickListener(new View.OnClickListener() {
+        //surfaceViewVisibilityButton = (Button) findViewById(R.id.surfaceview_visibility_button);
+       // surfaceViewVisibilityButton.setText("HIDE SURFACE VIEW");
+       /** surfaceViewVisibilityButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (cameraPreview.getVisibility() == View.VISIBLE) {
@@ -132,26 +159,43 @@ public class MainActivity extends Activity implements Detector.ImageListener, Ca
                 }
             }
         });
+        */
 
         detector = new CameraDetector(this, CameraDetector.CameraType.CAMERA_FRONT, cameraPreview);
         detector.setLicensePath("Affdex.license");
-        detector.setDetectSmile(true);
+        detector.setDetectAllEmojis(true);
         detector.setImageListener(this);
         detector.setOnCameraEventListener(this);
+        startDetector();
+        isSDKStarted = true;
     }
 
     @Override
     protected void onResume() {
+
         super.onResume();
+
+        // Logs 'install' and 'app activate' App Events.
+        AppEventsLogger.activateApp(this);
+
+
         if (isSDKStarted) {
             startDetector();
         }
+        else {
+            detector.reset();
+        }
+
+        screenTapped = false;
 
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+
+        // Logs 'app deactivate' App Event.
+        AppEventsLogger.deactivateApp(this);
         stopDetector();
 
     }
@@ -186,13 +230,18 @@ public class MainActivity extends Activity implements Detector.ImageListener, Ca
 
     @Override
     public void onImageResults(List<Face> list, Frame frame, float v) {
-        if (list == null)
-            return;;
-        if (list.size() == 0) {
-            smileTextView.setText("NO FACE");
-        } else {
-            Face face = list.get(0);
-            smileTextView.setText(String.format("SMILE\n%.2f",face.expressions.getSmile()));
+
+        if (!screenTapped) {
+            String text = "";
+
+            if (list == null)
+                return;;
+            if (list.size() != 0) {
+                Face face = list.get(0);
+                theEmoji = face.emojis.getDominantEmoji();
+                text = theEmoji.getUnicode();
+            }
+            smileTextView.setText(text);
         }
     }
 
@@ -206,5 +255,10 @@ public class MainActivity extends Activity implements Detector.ImageListener, Ca
             previewWidth = width;
         }
         cameraPreview.requestLayout();
+    }
+
+    public void screenTapped(View view) {
+        screenTapped = !screenTapped;
+        //Toast.makeText(getApplicationContext(), "Screen tapped", Toast.LENGTH_SHORT).show();
     }
 }
